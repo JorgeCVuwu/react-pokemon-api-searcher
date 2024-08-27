838a55779f7dc3dc2cf83e7fa14dbf60f8acd3e9
JorgeCVuwu <jorgecerda2008@gmail.com>
2024-08-27 14:38:56 -0400

    add functional NationalDex component

--- src/components/InputFilter.jsx
+++ src/components/InputFilter.jsx
@@ -80,7 +80,7 @@ export function InputFilter ({ name, filter, disabled, onChange, pokemonSearcher
       }
       {pokemonSearcher
         ? <p className='no-valid-campus' hidden={hideValidationError}>Please, insert a valid {name}.</p>
-        : <button type='submit' aria-label="search">
+        : <button type='submit' aria-label="search an article">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-search" viewBox="0 0 24 24">
               <circle cx="11" cy="11" r="8"></circle>
               <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
--- src/components/NationalDex.jsx
+++ src/components/NationalDex.jsx
@@ -1,16 +1,39 @@
 import { useContext } from 'react'
+import { useNationalDex } from '../hooks/useNationalDex.js'
 
 import { Link } from 'react-router-dom'
 import { PokemonPageContext } from '../context/pokemonPage'
 
-import { POKEAPI_PREFIX } from '../constants/constants'
+import '../styles/national-dex.css'
 
 export function NationalDex () {
-  const { pokemonDefaultData } = useContext(PokemonPageContext)
+  const { prevPokemonInfo, nextPokemonInfo } = useNationalDex()
+  const { pokemonDefaultData, pokemonSpeciesData } = useContext(PokemonPageContext)
 
-  return (
+  return prevPokemonInfo && nextPokemonInfo && (
     <div>
-        <img src={pokemonDefaultData.sprites.front_sprite} alt={`Front sprite of ${pokemonDefaultData.name}`}></img>
+      <p>{`#${pokemonSpeciesData.id}`}</p>
+      <div className='national-dex-container'>
+        <Link to={`/pokemon/${prevPokemonInfo.name}`}>
+          <div className='national-dex-attached-pokemon left'>
+            <p>#{prevPokemonInfo.id}</p>
+            <p>{prevPokemonInfo.name}</p>
+          </div>
+        </Link>
+
+        <div className='national-dex-current-container'>
+          <div className='national-dex-sprite-container'>
+            <img className='national-dex-sprite' src={pokemonDefaultData.sprites.front_sprite} alt={`Front sprite of ${pokemonDefaultData.name}`}></img>
+          </div>
+        </div>
+
+        <Link to={`/pokemon/${nextPokemonInfo.name}`}>
+          <div className='national-dex-attached-pokemon right'>
+            <p>{nextPokemonInfo.name}</p>
+            <p>#{nextPokemonInfo.id}</p>
+          </div>
+        </Link>
+      </div>
     </div>
   )
 }
